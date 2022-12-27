2be0a0220a5fa9e7a28da61ff180621b918ab63b
shinderu2u <amirulnazmi99@gmail.com>
2022-12-27 23:14:06 +0800

    increase and decrease button

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     //data, functions
     data(){
         return{
-            title: 'The Final Empire'
+            title: 'The Final Empire',
+            age: 10
         }
     }
 })
