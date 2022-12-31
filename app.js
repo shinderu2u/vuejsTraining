99ab3fd947358fbc21101c3f675ee536f33a2c67
shinderu2u <amirulnazmi99@gmail.com>
2022-12-31 20:46:18 +0800

    kotak

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const app = Vue.createApp({
     //data, functions
     data(){
         return{
+            showBooks : true,
             title: 'The Final Empire',
             age: 10
         }
-    }
+    },
+    methods: {
+        toggleHideBooks(){
+            this.showBooks = !this.showBooks
+        },
+
+        handleEvent(){
+            console.log('event')
+        }
+
+    },
 })
 
 app.mount('#app')
